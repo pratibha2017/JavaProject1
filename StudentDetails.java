1adc653f46b402560a5db15c1cfd2bed6ad39b04
pratibha2017 <pratibhahsolanki@gmail.com>
2017-07-20 23:40:43 +0100

    Initial commit

---
+++ StudentDetails.java
@@ -0,0 +1,85 @@
+/**
+ * Created by Welcome on 18/06/2017.
+ */
+public class StudentDetails {
+    //static method for student details
+    public static void main(String[]args){
+        System.out.println("Student = 1");
+        System.out.println("Nisha Patel");
+        System.out.println("Age: 26");
+        System.out.println("Roll Number: 1");
+        System.out.println("Nish`s Subject Economics,Psychology, Philosophy");
+        System.out.println("Economics = 78");
+        System.out.println("Psychology = 83");
+        System.out.println("Philosophy = 89");
+        System.out.println("Resuls is Distriction");
+        System.out.print("\n\n\n");
+
+
+
+        System.out.println("Student = 2");
+        System.out.println("Deepa Gandhi");
+        System.out.println("Age: 26");
+        System.out.println("Rollnumber: 2");
+        System.out.println("Radhika `s Subject Sunskrit, Hindi Gujrati");
+        System.out.println("Sunskrit = 87");
+        System.out.println("Hindi = 95");
+        System.out.println("Gujrati = 98");
+        System.out.println("Result is Very Good");
+        System.out.print("\n\n\n");
+
+
+
+        System.out.println("Student = 3");
+        System.out.println("Mohit Shah");
+        System.out.println("Age: 30");
+        System.out.println("Rollnumber: 3");
+        System.out.println("Mohit`s Subject`s Maths, English, Science");
+        System.out.println("Maths = 98");
+        System.out.println("English = 91");
+        System.out.println("Science = 95");
+        System.out.println("Result is very good");
+        System.out.print("\n\n\n");
+
+
+
+        System.out.println("Student = 4");
+        System.out.println("Keyur Desai");
+        System.out.println("Age: 28");
+        System.out.println("Roll Number: 4");
+        System.out.println("Keyur`s Subject Maths, English, Science");
+        System.out.println("Maths = 86");
+        System.out.println("English = 99");
+        System.out.println("Science = 98");
+        System.out.println("Result is very Good");
+        System.out.print("\n\n\n");
+
+
+        System.out.println("Student = 5");
+        System.out.println("Divya");
+        System.out.println("Age: 31");
+        System.out.println("Roll Number: 5");
+        System.out.println("Divya`s Subject Maths, English, Science");
+        System.out.println("Maths = 98");
+        System.out.println("English = 99");
+        System.out.println("Science = 96");
+        System.out.println("Result is very Good");
+        System.out.println("\n\n\n");
+
+
+
+        System.out.println("Student = 6");
+        System.out.println("Radhika");
+        System.out.println("Age: 26");
+        System.out.println("Roll Number: 6");
+        System.out.println("Radhika`s Subuject Gujarati, Hindi, Sanskrit");
+        System.out.println("Gujrati = 75");
+        System.out.println("Hindi = 65");
+        System.out.println("Sanskrit = 55");
+        System.out.println("Result is Not Good ");
+
+
+
+
+        }
+}
